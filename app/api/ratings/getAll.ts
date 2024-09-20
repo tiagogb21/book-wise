0f0e29c64845b2b4a83ba473b679f83dab15e02d
Tiago Garbi <tgbmateriais@gmail.com>
2024-09-20 20:11:48 -0300

    fix: problem with rating order

--- app/api/ratings/getAll.ts
+++ app/api/ratings/getAll.ts
@@ -11,7 +11,7 @@ export async function getRatings(): Promise<ExtendedRating[]> {
             book: true,
         },
         orderBy: {
-            rate: "desc",
+            created_at: "desc",
         },
     });
 
